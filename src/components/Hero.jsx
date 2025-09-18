5519bcaf25274f95a12044a193f4d9a215c6880d
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-09-18 19:22:00 +0530

    feat: add Hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import heroImg from '../assets/hero.jpg'
+
+function Hero() {
+    return (
+        <div className='relative'>
+            <img src={heroImg} alt="Hero" />
+            <button className='absolute bottom-[30px] left-1/2 bg-white rounded-full be-vietnam-pro-bold uppercase py-[10px] px-10 text-[14px] transform -translate-x-1/2 '>Shop New</button>
+        </div>
+    )
+}
+
+export default Hero
