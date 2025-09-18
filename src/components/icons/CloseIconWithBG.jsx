e6b1213ec087bb6f95ee6bab81f861e685ea7d09
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-09-18 19:19:29 +0530

    fix: fix DOM error of svg  icons

--- src/components/icons/CloseIconWithBG.jsx
+++ src/components/icons/CloseIconWithBG.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CloseIconWithBG() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-x" style="stroke-width: 3px;"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" strokeWidth="2" stroke="white" fill="none" strokeinecap="round" strokeLinejoin="round" className="bg-[#262626] rounded-full w-[23px] h-[23px]" ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
     )
 }
 
--- src/components/icons/HamBurgerIcon.jsx
+++ src/components/icons/HamBurgerIcon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HamBurgerIcon() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="34" height="34" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="34" height="34" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
             <line x1="4" y1="6" x2="20" y2="6"></line>
             <line x1="4" y1="12" x2="20" y2="12"></line>
