cbe9f6b95d2776c86966cc496e604c8fd39ff9e7
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-09-18 20:50:13 +0530

    feat: add cart qty inside cart icon

--- src/components/icons/CartIcons.jsx
+++ src/components/icons/CartIcons.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function CartIcons() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 30">
-            <path d="M24.4651 7.53777V5.45974H18.4222V0.730133H6.56077V5.45974H0.586426V29.2699H24.3965V11.7022H22.3185V27.1918H2.66446V7.53777H24.4651ZM8.63881 5.45974V2.80817H16.3442V5.45974H8.63881Z" fill="currentColor"></path>
-        </svg>
+        <div className='relative'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 30">
+                <path d="M24.4651 7.53777V5.45974H18.4222V0.730133H6.56077V5.45974H0.586426V29.2699H24.3965V11.7022H22.3185V27.1918H2.66446V7.53777H24.4651ZM8.63881 5.45974V2.80817H16.3442V5.45974H8.63881Z" fill="currentColor"></path>
+            </svg>
+            <span className='absolute be-vietnam-pro-semibold top-[30%] left-1/2  -translate-x-1/2 text-[10px]'>0</span>
+        </div>
     )
 }
 
