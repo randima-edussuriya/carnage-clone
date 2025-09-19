222e12931256346923631a3c8c9ba2206a1be4ee
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-09-19 18:01:18 +0530

    feat: add cursor-pointer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     return (
         <div className='relative'>
             <img src={heroImg} alt="Hero" />
-            <button className='absolute bottom-[30px] left-1/2 bg-white rounded-full be-vietnam-pro-bold uppercase py-[10px] px-10 text-[14px] transform -translate-x-1/2 '>Shop New</button>
+            <button className='absolute bottom-[30px] left-1/2 bg-white rounded-full be-vietnam-pro-bold uppercase py-[10px] px-10 text-[14px] transform -translate-x-1/2 cursor-pointer'>Shop New</button>
         </div>
     )
 }
