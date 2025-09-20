97fa252ba5f895b96d6c0e61c62bec85f9c73e64
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-09-20 07:29:45 +0530

    fix: fix DOM error

--- src/components/icons/PlusIcon.jsx
+++ src/components/icons/PlusIcon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PlusIcon() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icon-tabler-plus cursor-pointer"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
     )
 }
 
