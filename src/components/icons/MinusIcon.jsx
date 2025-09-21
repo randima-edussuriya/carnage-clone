3703ae474d1403ee718efda733fe4d675f1e0cff
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-09-21 21:29:53 +0530

    feat: add minus icon

---
+++ src/components/icons/MinusIcon.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function MinusIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icon-tabler-minus"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <line x1="5" y1="12" x2="19" y2="12"></line>
+    </svg>
+  );
+}
+
+export default MinusIcon;
